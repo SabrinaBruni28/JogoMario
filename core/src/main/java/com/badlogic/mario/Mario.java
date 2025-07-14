cce5befcdbebdec8dc751d94115cd0638dcb2e80
SabrinaBruni28 <sabrina.b.faria@ufv.br>
2025-07-14 16:15:05 -0300

    Ajuste do Mario

--- core/src/main/java/com/badlogic/mario/Mario.java
+++ core/src/main/java/com/badlogic/mario/Mario.java
@@ -9,17 +9,18 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Mario {
     private float stateTime = 0f; // Tempo de estado da animação
-    private static final int FRAME_WIDTH = 30;  // Largura de cada sprite
-    private static final int FRAME_HEIGHT = 30; // Altura de cada sprite
+    private static final int FRAME_WIDTH = 29;  // Largura de cada sprite
+    private static final int FRAME_HEIGHT = 40; // Altura de cada sprite
+    private static final int HEIGHT_Y = 172;
     private Texture spriteSheet;
 
     private enum State {
-        STANDING_RIGHT, STANDING_LEFT, STANDING_DOWN, WALKING_RIGHT, WALKING_LEFT
+        STANDING_RIGHT, STANDING_LEFT, STANDING_DOWN_RIGHT, STANDING_DOWN_LEFT, WALKING_RIGHT, WALKING_LEFT, JUMPING_RIGHT, JUMPING_LEFT
     }
 
     private float posX = 10; // Posição inicial do personagem no eixo X
+    private float posY = HEIGHT_Y; // Posição inicial do personagem no eixo Y
 
-    private float posY = 100; // Posição inicial do personagem no eixo Y
     private float widthMario = 150;
     private float heightMario = 200;
     private float speed = 200; // Velocidade de movimento do personagem (pixels por segundo)
@@ -28,16 +29,13 @@ public class Mario {
     private float velocityY = 2f; // Velocidade vertical do personagem
     private float gravity = -800f; // Aceleração da gravidade (negativa porque puxa para baixo)
     private float jumpHeight = 400f; // A altura do pulo
-
-    private boolean isCrouching = false; // Verifica se o personagem está abaixado
-    private float crouchHeight = 100f; // A altura do personagem quando abaixado
+    private boolean sideRight = true;
 
     private State currentState = State.STANDING_RIGHT; // Estado atual do personagem
-    private Animation<TextureRegion> standRightAnimation;
-    private Animation<TextureRegion> standLeftAnimation;
-    private Animation<TextureRegion> standDownAnimation;
-    private Animation<TextureRegion> walkRightAnimation;
-    private Animation<TextureRegion> walkLeftAnimation;
+    private Animation<TextureRegion> standRightAnimation, standLeftAnimation;
+    private Animation<TextureRegion> standDownRightAnimation, standDownLeftAnimation;
+    private Animation<TextureRegion> walkRightAnimation, walkLeftAnimation;
+    private Animation<TextureRegion> jumpingLeftAnimation, jumpingRightAnimation;
 
     public Mario() {
         // Carregar a folha de sprites
@@ -53,14 +51,24 @@ public class Mario {
         standLeftAnimation = new Animation<>(0.4f, spriteRegions[0][6]);
         standLeftAnimation.setPlayMode(Animation.PlayMode.NORMAL);
 
-        standDownAnimation = new Animation<>(0.4f, spriteRegions[0][6]);
-        standDownAnimation.setPlayMode(Animation.PlayMode.NORMAL);
+        TextureRegion finalFrame = new TextureRegion(spriteSheet, 13 * FRAME_WIDTH, 0, FRAME_WIDTH, FRAME_HEIGHT);
+        standDownRightAnimation = new Animation<>(0.4f, finalFrame);
+        standDownRightAnimation.setPlayMode(Animation.PlayMode.NORMAL);
+
+        standDownLeftAnimation = new Animation<>(0.4f, spriteRegions[0][0]);
+        standDownLeftAnimation.setPlayMode(Animation.PlayMode.NORMAL);
 
         walkRightAnimation = new Animation<>(0.4f, spriteRegions[0][9], spriteRegions[0][10]);
         walkRightAnimation.setPlayMode(Animation.PlayMode.LOOP);
 
         walkLeftAnimation = new Animation<>(0.4f, spriteRegions[0][3], spriteRegions[0][4]);
         walkLeftAnimation.setPlayMode(Animation.PlayMode.LOOP);
+
+        jumpingLeftAnimation = new Animation<>(0.4f, spriteRegions[0][1]);
+        jumpingLeftAnimation.setPlayMode(Animation.PlayMode.NORMAL);
+
+        jumpingRightAnimation = new Animation<>(0.4f, spriteRegions[0][12]);
+        jumpingRightAnimation.setPlayMode(Animation.PlayMode.NORMAL);
     }
 
     public void dispose() {
@@ -69,6 +77,7 @@ public class Mario {
 
     public void input(float delta, Cenario cenario, float screenWidth, float screenHeight) {
         boolean isMoving = false;
+        if (isJumping) isMoving = true;
 
         // Verificar se o personagem saiu dos limites da tela e ajustar a posição
         
@@ -81,7 +90,10 @@ public class Mario {
         // Movimento para a esquerda
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
             andarEsquerda(delta, screenWidth);
-            currentState = State.WALKING_LEFT;
+            if (isJumping)
+                currentState = State.JUMPING_LEFT;
+            else
+                currentState = State.WALKING_LEFT;
             isMoving = true;
         }
 
@@ -95,13 +107,22 @@ public class Mario {
             else {
                 cenario.setMoviment(false);
             }
-            currentState = State.WALKING_RIGHT;
+
+            if (isJumping)
+                currentState = State.JUMPING_RIGHT;
+            else
+                currentState = State.WALKING_RIGHT;
             isMoving = true;
         }
 
 
         // Se o personagem não estiver pulando e a tecla de pulo estiver pressionada
         if (Gdx.input.isKeyPressed(Input.Keys.SPACE) && !isJumping) {
+            if (!sideRight)
+                currentState = State.JUMPING_LEFT;
+            else
+                currentState = State.JUMPING_RIGHT;
+            
             velocityY = jumpHeight; // Inicia o pulo
             isJumping = true;
             isMoving = true;
@@ -113,20 +134,19 @@ public class Mario {
 
         // Verificar se o personagem deve abaixar
         else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-            isCrouching = true; // Marca o personagem como abaixado
-            posY = 150;
+            isMoving = true;
+            if (!sideRight)
+                currentState = State.STANDING_DOWN_LEFT;
+            else
+                currentState = State.STANDING_DOWN_RIGHT;
         } 
-        else {
-            isCrouching = false; // Marca o personagem como não abaixado
-            posY = 100;
-        }
 
         // Se não estiver se movendo, definir o estado como parado
         if (!isMoving) {
-            if (currentState == State.WALKING_RIGHT) {
+            if (sideRight) {
                 currentState = State.STANDING_RIGHT;
             } 
-            else if (currentState == State.WALKING_LEFT) {
+            else {
                 currentState = State.STANDING_LEFT;
             }
         }
@@ -139,27 +159,47 @@ public class Mario {
         TextureRegion currentFrame = null;
         switch (currentState) {
             case STANDING_RIGHT:
+                posY = HEIGHT_Y;
                 currentFrame = standRightAnimation.getKeyFrame(stateTime, false);
+                sideRight = true;
                 break;
             case STANDING_LEFT:
+                posY = HEIGHT_Y;
                 currentFrame = standLeftAnimation.getKeyFrame(stateTime, false);
+                sideRight = false;
                 break;
-            case STANDING_DOWN:
-                currentFrame = standDownAnimation.getKeyFrame(stateTime, false);
+            case STANDING_DOWN_RIGHT:
+                posY = HEIGHT_Y - 25;
+                currentFrame = standDownRightAnimation.getKeyFrame(stateTime, false);
+                sideRight = true;
+                break;
+            case STANDING_DOWN_LEFT:
+                posY = HEIGHT_Y - 25;
+                currentFrame = standDownLeftAnimation.getKeyFrame(stateTime, false);
+                sideRight = false;
                 break;
             case WALKING_RIGHT:
+                posY = HEIGHT_Y;
                 currentFrame = walkRightAnimation.getKeyFrame(stateTime, true);
+                sideRight = true;
                 break;
             case WALKING_LEFT:
+                posY = HEIGHT_Y;
                 currentFrame = walkLeftAnimation.getKeyFrame(stateTime, true);
+                sideRight = false;
+                break;
+            case JUMPING_RIGHT:
+                currentFrame = jumpingRightAnimation.getKeyFrame(stateTime, true);
+                sideRight = true;
+                break;
+            case JUMPING_LEFT:
+                currentFrame = jumpingLeftAnimation.getKeyFrame(stateTime, true);
+                sideRight = false;
                 break;
         }
 
-        // Se o personagem estiver abaixado, desenha com uma altura reduzida
-        float drawHeight = isCrouching ? crouchHeight : heightMario;
-
         batch.begin();
-            batch.draw(currentFrame, posX, posY, widthMario, drawHeight); // Desenhar na posição atual
+            batch.draw(currentFrame, posX, posY, widthMario, heightMario); // Desenhar na posição atual
         batch.end();
     }
 
@@ -179,8 +219,8 @@ public class Mario {
         posY += velocityY * delta; // Atualiza a posição vertical
 
         // Quando o personagem tocar o chão, parar o pulo
-        if (posY <= 100) {
-            posY = 100; // Garante que o personagem não passe do chão
+        if (posY <= HEIGHT_Y) {
+            posY = HEIGHT_Y; // Garante que o personagem não passe do chão
             isJumping = false; // O pulo foi finalizado
             velocityY = 0f; // Reseta a velocidade vertical
         }
